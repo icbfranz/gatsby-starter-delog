3fb851f44d8d2a29983b372486dc60b8c9aadf19
Bruce Franz <48036781+icbfranz@users.noreply.github.com>
2021-02-01 22:28:03 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} AdvTech &bull; Crafted with <span role="img" aria-label="love">❤️</span> by Bruce</a></p>
+        <p>&copy; {new Date().getFullYear()} AdvTech &bull; Crafted with <span role="img" aria-label="love">❤️</span> by Bruce</p>
       </footer>
     </div>
   )
