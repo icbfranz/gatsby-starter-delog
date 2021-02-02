b30274a4276f492762937a8753100140f5f34881
Bruce Franz <48036781+icbfranz@users.noreply.github.com>
2021-02-01 23:08:31 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://use.typekit.net/jzs1ced.css">
+</head>
+</html>
 import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby'
